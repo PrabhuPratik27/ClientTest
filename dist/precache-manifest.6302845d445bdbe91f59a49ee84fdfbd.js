8168ae8ee0e2cef2ffb7d13180e4b5d4168dab40
Pratik <prabhupratik27@gmail.com>
2019-04-22 21:03:02 +0530

    Added dist

---
+++ dist/precache-manifest.6302845d445bdbe91f59a49ee84fdfbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "214a92d90c6b8d1810c4",
+    "url": "/js/app.3973ac6a.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "6c1a3911beb0af046f3ebb9cac3db5ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "214a92d90c6b8d1810c4",
+    "url": "/css/app.80d62a31.css"
+  }
+];
